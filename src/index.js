fc5b3e798664fada3fffd858048caa596c9b0886
Siarhei Kvachonak <segrey1989@tut.by>
2019-05-18 16:09:31 +0300

    :feat: create a redux Provider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers/rootReducer';
+import firebase from './firebase';
+import { createFirestoreInstance } from 'redux-firestore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: 'users',
+    useFirestoreForProfile: true,
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance, // <- needed if using firestore
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+
+  document.getElementById('root'),
+);
 
 serviceWorker.unregister();
