8a1852d82b3aeaec7d7f9bef9c022ee5de150503
Siarhei Kvachonak <segrey1989@tut.by>
2019-05-18 16:02:18 +0300

    feat: main component to show events list

--- src/components/Events/Dashboard.js
+++ src/components/Events/Dashboard.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import EventList from './EventList';
+import { Grid, Container, Header } from 'semantic-ui-react';
 
 class Dashboard extends Component {
   render() {
-    return <h2>Dashboard</h2>;
+    return (
+      <Container>
+        <Header as='h2' textAlign='center'>
+          Upcoming Events
+        </Header>
+        <Grid>
+          <EventList events={this.props.events} user={this.props.user} />
+        </Grid>
+      </Container>
+    );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = state => {
+  const { events } = state.firestore.ordered;
+  const { users } = state.firestore.data;
+  const { uid } = state.firebase.auth;
+  const user = users ? users[uid] : null;
+  return {
+    events,
+    user,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'events' }, { collection: 'users' }]),
+)(Dashboard);
