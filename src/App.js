83811058fbba491f65a621a17873a21da89f5beb
Siarhei Kvachonak <segrey1989@tut.by>
2019-05-18 16:06:02 +0300

    :feat: display components after getting auth status (for correct displaying navBar links)

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
 import Dashboard from './components/Events/Dashboard';
 import NavBar from './components/navigation/NavBar';
+import AddEvent from './components/Events/AddEvent';
+import BookEventModal from './components/Events/bookEventModal';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route exact path='/' component={Dashboard} />
-        <Route path='/register' component={Register} />
-        <Route path='/login' component={Login} />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    const { auth } = this.props;
+    if (auth.isLoaded) {
+      return (
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/register' component={Register} />
+            <Route path='/login' component={Login} />
+            <Route path='/addEvent' component={AddEvent} />
+            <Route path='/bookEvent' component={BookEventModal} />
+          </Switch>
+        </BrowserRouter>
+      );
+    }
+    return null;
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
+
+export default connect(mapStateToProps)(App);
