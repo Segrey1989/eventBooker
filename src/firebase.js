146efe64efc25e93df361a488c3fe301a2fdae58
Siarhei Kvachonak <segrey1989@tut.by>
2019-05-18 16:07:13 +0300

    :feat: firebase configuration and initialization

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyB08GID8dAzwpFYzXufvtcqCzCnnvw8Zu4',
+  authDomain: 'event-booker-ab765.firebaseapp.com',
+  databaseURL: 'https://event-booker-ab765.firebaseio.com',
+  projectId: 'event-booker-ab765',
+  storageBucket: 'event-booker-ab765.appspot.com',
+  messagingSenderId: '63792606940',
+  appId: '1:63792606940:web:37c712f28b43728e',
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
